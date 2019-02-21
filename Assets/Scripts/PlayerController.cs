c2b8ae0770d31f4de704e79747ef424731d17d65
Enerlo <svkolychev@yandex.ru>
2019-02-21 21:46:25 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enerlion
+{
+    [RequireComponent(typeof(UnitMotor))]
+    public class PlayerController : MonoBehaviour
+    {
+        private Camera _camera;
+        private UnitMotor _motor;
+
+        [SerializeField] private LayerMask _movementMask;
+
+        void Start()
+        {
+            _camera = Camera.main;
+            _motor = GetComponent<UnitMotor>();
+        }
+        
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, 100f, _movementMask))
+                {
+                    _motor.MoveToPoint(hit.point);
+                }
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, 100f))
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UnitMotor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Enerlion
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class UnitMotor : MonoBehaviour
+    {
+        NavMeshAgent _agent;
+
+        void Start()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+        }
+
+        public void MoveToPoint(Vector3 point)
+        {
+            _agent.SetDestination(point);
+        }
+    }
+}
